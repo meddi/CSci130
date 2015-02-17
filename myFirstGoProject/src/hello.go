9bb8faee102a51fdfeac2f0c34c5f40228c30d33
medi_home <mehdix@mail.fresnostate.edu>
2015-02-16 23:36:43 -0800

    Web app with different URL paths

---
+++ myFirstGoProject/src/hello.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+    "net/http"
+    "fmt"
+    
+    "appengine"
+    "appengine/user"
+)
+
+// Default Request Handler
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", defaultHandler)
+    http.ListenAndServe(":8080", nil)
+}
+
